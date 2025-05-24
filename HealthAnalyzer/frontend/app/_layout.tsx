52bc7d732a7d4a8f28203c060f4eb6ade5b1bf65
Hj-codes <hjcodes17@gmail.com>
2025-05-24 16:26:48 +0530

    harsh tama setup

--- HealthAnalyzer/frontend/app/_layout.tsx
+++ HealthAnalyzer/frontend/app/_layout.tsx
@@ -7,6 +7,8 @@ import { useEffect } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { AuthProvider, useAuth } from '@/context/AuthContext';
+import { TamaguiProvider } from 'tamagui'
+import tamaguiConfig from '@/tamagui.config'
 
 function RootLayoutNav() {
     const { user, isLoading } = useAuth();
@@ -29,20 +31,24 @@ function RootLayoutNav() {
     }
   
     return (
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(app)" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+     <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <Stack>
+            <Stack.Screen name="(app)" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+            </Stack>
+            <StatusBar style="auto" />
+        </ThemeProvider>
+     </TamaguiProvider>
     );
 }
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    // SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
--- HealthAnalyzer/frontend/metro.config.js
+++ HealthAnalyzer/frontend/metro.config.js
@@ -1,7 +1,7 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
-config.resolver.sourceExts.push('cjs');
+const config = getDefaultConfig(__dirname, { isCSSEnabled: true });
+config.resolver.sourceExts.push('mjs');
 config.resolver.unstable_enablePackageExports = false;
 
 module.exports = config;
