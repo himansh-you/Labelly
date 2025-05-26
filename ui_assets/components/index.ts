8ce1564bd9bef3fb38264ae6de163fb2d4a3d3f9
Harsh Jain <131331439+Hj-codes@users.noreply.github.com>
2025-05-26 19:26:38 +0530

    Delete ui_assets directory

--- ui_assets/components/index.ts
+++
@@ -1,9 +0,0 @@
-// Export all components here for clean imports
-export { Logo } from './Logo'
-export { GoogleIcon } from './GoogleIcon'
-
-// Example:
-// export { default as Button } from './Button'
-// export { default as Header } from './Header'
-
-// Add your component exports here as you create them 
